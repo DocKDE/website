ecb3c2accd7dd487bdefbe7fc417987669e40928
Benedikt Flöser <benedikt.floeser@cec.mpg.de>
2022-03-28 17:20:11 +0200

    overhaul to show projects and update skills

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Usage: All arguments passed to the script will be interpreted as the commit message
 
-cd ~/Documents/code/hugo/portfolio
+cd `pwd`
 rm -r public && hugo --minify
 git add -A
-git commit -m "@$"
+git commit -m "$1"
 # git subtree push --prefix public origin master
 git push github master
